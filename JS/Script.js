fc44ef8c857fe17b70cb25153f4ce8a0e36cc4df
Gayatri19670 <samirgupta19670@gmail.com>
2020-10-12 21:39:36 +0530

    JS file

--- JS/Script.js
+++ JS/Script.js
@@ -1,8 +1,7 @@
 const screen = document.getElementById('screen');
-let value1 = '';
-false;
-let value2 = '';
-false;
+let value1 = ''; // false;
+let value2 = ''; //false;
+let value3 = ''; // false;
 let operation = '+';
 let operPressed = false;
 let total = 0;
@@ -11,55 +10,65 @@ let total = 0;
 function onNumClick(value) {
     value = value.toString();
     if (operPressed) {
-        if (!value2) value2 = '';
         value2 = value2 + value;
         screen.innerHTML = value1 + operation + value2;
     } else {
-        if (!value1) value1 = '';
         value1 = value1 + value;
         screen.innerHTML = value1;
     }
-    value1 = value1 + value;
-    screen.innerHTML = value1;
     console.log(value);
 }
 
 function onOprationClick(value) {
-    operPressed = true;
-    operation = value;
-    let privousValue = screen.innerHTML.toString();
-    screen.innerHTML = privousValue + value;
-    console.log(value);
+    if (operPressed === false) {
+        value = value.toString();
+        operPressed = true;
+        operation = value;
+        let privousValue = screen.innerHTML.toString();
+        screen.innerHTML = privousValue + value;
+        console.log(value, operPressed);
+    } else {
+        value = value.toString();
+        console.log('else', value);
+        operPressed = true;
+        operation = value;
+        let privousValue = screen.innerHTML.toString();
+        const newPrivousValue = privousValue.substring(0, privousValue.length - 1);
+        screen.innerHTML = newPrivousValue + value;
+        console.log(value, operPressed);
+    }
 }
 
 function totalValue() {
-    if (value1 && value2) {
-        total = calculateTotalValue(parseInt(value1), parseInt(value2), operation);
-    }
-    total = total.toString();
-    screen.innerHTML = total;
-    console.log(value1, value2, operation);
-}
-
-function calculateTotalValue(value1, value2, operation) {
     switch (operation) {
         case '+':
-            total = value1 + value2;
+            total = parseInt(value1) + parseInt(value2);
             break;
         case '-':
-            total = value1 - value2;
+            total = parseInt(value1) - parseInt(value2);
             break;
         case '/':
-            total = value1 / value2;
+            total = parseInt(value1) / parseInt(value2);
             break;
         case '*':
-            total = value1 * value2;
+            total = parseInt(value1) * parseInt(value2);
             break;
         default:
             return false;
     }
-    return total;
+    total = total.toString();
+    screen.innerHTML = total;
+
+    if (operPressed) {
+        total = total + value;
+        screen.innerHTML = total + operation + value;
+    } else {
+        total = total + value;
+        screen.innerHTML = total;
+    }
 }
+
+
 
 //function that clear the display 
 function onOprationClick1() {
